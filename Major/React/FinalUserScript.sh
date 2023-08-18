0f5b7d34f220a969cee5f29f791e4fb803f44183
Rajeev Singh Jadon <jadonrajeevsingh237@gmail.com>
2023-08-18 14:18:58 +0530

    Organizing things...

---
+++ Major/React/FinalUserScript.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+sudo su
+sudo yum install -y nodejs
+sudo yum install -y nginx
+sudo yum install -y git
+sudo systemctl enable nginx
+systemctl status nginx
+cd /usr/share/nginx/html
+git clone https://github.com/shreyanshrathore/My-chat.git
+cd My-chat
+npm install
+npm run build
+cd /etc/nginx/
+
+sudo su
+rm -f nginx.conf 
+
+sudo echo "user nginx;
+worker_processes auto;
+error_log /var/log/nginx/error.log notice;
+pid /run/nginx.pid;
+
+# Load dynamic modules. See /usr/share/doc/nginx/README.dynamic.
+include /usr/share/nginx/modules/*.conf;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile            on;
+    tcp_nopush          on;
+    keepalive_timeout   65;
+    types_hash_max_size 4096;
+
+    include             /etc/nginx/mime.types;
+    default_type        application/octet-stream;
+      include /etc/nginx/conf.d/*.conf;
+
+    server {
+        listen       80;
+        listen       [::]:80;
+        server_name  _;
+        root         /usr/share/nginx/html/My-chat/build;
+
+        # Load configuration files for the default server block.
+        include /etc/nginx/default.d/*.conf;
+
+        error_page 404 /404.html;
+        location = /404.html {
+        }
+
+        error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+        }
+    }
+}   " >> nginx.conf
+
+
+sudo service nginx restart
